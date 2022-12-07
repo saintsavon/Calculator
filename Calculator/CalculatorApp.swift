aa524777ae12569166a2f7d5f13a0d2947e84057
saintsavon <jaylensavon@gmail.com>
2022-12-07 00:17:27 -0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jaylen Scott on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
